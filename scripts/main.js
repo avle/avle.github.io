df5643620460cf224ffe719cb02d0e6eef53f384
avle <avle@users.noreply.github.com>
2016-01-13 17:15:26 -0800

    modify heading 1 line

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
+/*
 var myHeading = document.querySelector('h1');
 var myButton = document.querySelector('button');
 
 myHeading.innerHTML = 'Hello World!';
+*/
+
+document.querySelector('h1').innerHTML = 'Hello World!';
